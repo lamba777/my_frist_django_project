4b1ac914af2bb60ade83553d9f29dd5ea62ea3e1
Artem Belskiy <lamba777a@gmail.com>
2023-02-28 17:50:57 +0200

    Added slug, added view - archive, added redirect

--- coolsite/settings.py
+++ coolsite/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yv8!op^iqbf2q66(ot(wbd*ibeg#$y(nlxm5*9r)yq=1@1!qh('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
+
 
 
 # Application definition
